f952c3a00269d5a6b6215fd63d54fc3945b815ab
askiji <askijipro@gmail.com>
2023-01-31 17:57:58 +0100

    final

--- src/Servidor/ReenviarPaquete.java
+++ src/Servidor/ReenviarPaquete.java
@@ -7,13 +7,9 @@ public class ReenviarPaquete {
 	
 	
 	public static void reenviarTodos(String texto, Integer puerto){ 
-//		System.out.println(texto);
-//		System.out.println(puerto.toString());
-	//	Puertos.interPuertos.forEach((a,b) -> System.out.println(a +"AAAAAAAAAA"+b));
+
 		Puertos.interPuertos.forEach((a , b ) -> {
-			System.out.println(a + "-------"+ puerto);
 			if(!a.equals(puerto)) {
-				System.out.println(a +"+++++++"+ puerto);
 				new Paquete().paquetesParaTodos(texto,b, a );
 			}
 		}) ;
